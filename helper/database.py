08f599ca96f22d6896f5858e5c879f10cb136bac
animepahe69 <tanjirokamado66999@gmail.com>
2024-09-18 16:51:16 +0530

    Update database.py

--- helper/database.py
+++ helper/database.py
@@ -19,7 +19,7 @@ class Database:
             prefix=None,
             suffix=None,
             metadata=False,
-            metadata_code=""" -map 0 -c:s copy -c:a copy -c:v copy -metadata title="Powered By:- @Kdramaland" -metadata author="@Snowball_Official" -metadata:s:s title="Subtitled By :- @Kdramaland" -metadata:s:a title="By :- @Kdramaland" -metadata:s:v title="By:- @Snowball_Official" """
+            metadata_code=""" -map 0 -c:s copy -c:a copy -c:v copy -metadata title="Powered By:- @Anime_Bloodline" -metadata author="@Anime_Bloodline" -metadata:s:s title="Subtitled By :- @Anime_Bloodline" -metadata:s:a title="By :- @Anime_Bloodline" -metadata:s:v title="By:- @Anime_Bloodline" """
         )
 
     async def add_user(self, b, m):
